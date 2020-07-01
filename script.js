aa4755e5db972df547c0704f33612796e1d6b383
josiehpersson <josephine.h.persson@outlook.com>
2020-07-01 10:33:07 +0200

    setup html/js

---
+++ script.js
@@ -0,0 +1,4 @@
+const questionContainer = document.getElementById('quiz-question-container');
+const answersContainer = document.getElementById('quiz-answers-container');
+const submitBtn = document.getElementById('submit-quiz-answer-btn');
+const quitBtn = document.getElementById('quit-quiz-btn');
